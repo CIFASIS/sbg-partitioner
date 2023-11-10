884eb8e2c551e2d094c82dcd017f1800d9ebaac1
fran <fsansone@irobot.com>
2023-11-10 14:01:57 -0300

    build a graph for an expresion for with single variable

--- src/build_sb_graph.hpp
+++ src/build_sb_graph.hpp
@@ -1,3 +1,28 @@
+/*****************************************************************************
+
+ This file is part of SBG Partitioner.
+
+ SBG Partitioner is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ SBG Partitioner is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with SBG Partitioner.  If not, see <http://www.gnu.org/licenses/>.
+
+ ******************************************************************************/
+
+#pragma once
+
 #include <sbg/sbg.hpp>
 
+/// Takes a path to a json file, reads it and then builds a sb graph with
+/// a node for each access to a variable and an edge for each connection
+/// between variables.
+/// If a variable appears on the left and on the right side, an edge is created.
 SBG::LIB::CanonSBG build_sb_graph(const char* filename);
