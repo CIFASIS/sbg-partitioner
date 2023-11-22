5d4c0468c351081766dc7ac1edea015be07aff5a
fran <fsansone@irobot.com>
2023-11-22 13:37:12 -0300

    added some documentation

--- src/partition_graph.hpp
+++ src/partition_graph.hpp
@@ -28,26 +28,44 @@
 
 namespace sbg_partitioner {
 
+/// This class represents a partioned set based graph.
+/// The main purpose of PartitionGraph is to divide the
+/// calculation of different nodes of a dynamic model
+/// in order to simulate it in parallel.
 class PartitionGraph {
 
 public: 
     PartitionGraph(SBG::LIB::CanonSBG& _graph, int number_of_partitions);
 
+    /// Returns a reference of the set based graph to be partioned.
     const SBG::LIB::CanonSBG& graph() const;
 
+    /// Assigns a partition to a interval.
+    /// @param interval_index Index of the interval
+    /// @param partition_number id of the partition
     void set_partition(size_t interval_index, size_t partition_number);
 
+    /// Returns the current partion as a map where each interval is associated
+    /// to a partition number.
     std::map<SBG::LIB::SetPiece, int> partitions() const;
 
+    /// Returns the connectivity set of a set of edges contained in map1 and map2 of
+    /// the graph (I mean, edges in CanonSBG::map1()[edge_index] and CanonSBG::map2()[edge_index]).
+    /// So that, we consider the graph as an undirected graph.
     std::unordered_set<size_t> get_connectivity_set(size_t edge_index) const;
 
 private:
     SBG::LIB::CanonSBG _graph;
     std::map<SBG::LIB::SetPiece, int> _partitions;
 
+    /// It creates an initial partition.
     void make_initial_partition(int number_of_partitions);
 };
 
+/// This function returns the cardinality of a connectivity set.
+/// @param pgraph graph partition
+/// @param edge_index index of the edges to get the connectivity set.
+/// @return the cardinality of the connectivity set
 size_t connectivity_set_cardinality(const PartitionGraph& pgraph, size_t edge_index);
 
 std::ostream& operator<<(std::ostream& os, const PartitionGraph pgraph);
